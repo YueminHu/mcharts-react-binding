268efd549908b9bb0b3c36b2264007aeae703116
Yuemin.Hu <hym920408@gmail.com>
2019-06-20 17:00:55 +0800

    add docs output

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import './styles/style.module.less';
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Router>
+      <Router basename={'/mcharts-react-binding'}>
         <Component />
       </Router>
     </AppContainer>,
